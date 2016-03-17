dd81aee326947280a22a034d5d585e2b2186e98c
Jan Ruehling <janruehling@gmail.com>
2016-03-17 19:19:30 +0100

    Fix deploy script

---
+++ jsonServer.js
@@ -0,0 +1,8 @@
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+server.use(jsonServer.defaults())
+
+var router = jsonServer.router('db.json')
+server.use(router)
+
+server.listen(3000)
